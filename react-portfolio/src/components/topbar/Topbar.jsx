a473ec8cde034f5ce634016ba938a84b59efc238
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-09-26 04:41:41 -0700

    import icons from the package

--- react-portfolio/src/components/topbar/Topbar.jsx
+++ react-portfolio/src/components/topbar/Topbar.jsx
@@ -1,15 +1,29 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import {Person,Email} from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro">Hello</a>
+                    <a href="#header" className="logo">fire.</a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>+451 328 5678</span>
+                        < div className="itemContainer">
+                            <Email className="icon" />
+                            <span>christiangflores0824@gmail.com</span>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                            <span className="line1"> </span>
+                            <span className="line2"> </span>
+                            <span className="line3"> </span>
+                        </div>
                 </div>
-                <div className="right"></div>
             </div>
-            
         </div>
-    )
+     </div>  
+    )     
 }
