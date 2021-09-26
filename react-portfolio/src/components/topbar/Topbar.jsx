012412d66500d2e9d2f3055ef7f7a2d1d7ec5ce1
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-09-26 00:09:04 -0700

    added wrapper and linked intro

--- react-portfolio/src/components/topbar/Topbar.jsx
+++ react-portfolio/src/components/topbar/Topbar.jsx
@@ -3,6 +3,12 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro">Hello</a>
+                </div>
+                <div className="right"></div>
+            </div>
             
         </div>
     )
