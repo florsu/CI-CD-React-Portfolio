1238fcd11984a06ade3c3caf7c38c09b3e4c5f8f
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-09-26 00:55:13 -0700

    import & populate sections

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,10 +1,23 @@
 import Topbar from "./components/topbar/Topbar";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+
 
 function App() {
   return (
     <div className="app">
       <Topbar/>
-      
+      <div className="sections">
+        <Header/>
+        <Home/>
+        <Portfolio/>
+        <Works/>
+        <Contact/>
+
+      </div>
       
     </div>
   );
