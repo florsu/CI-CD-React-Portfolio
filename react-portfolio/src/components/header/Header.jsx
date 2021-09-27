81a58ae4d36ce0831cf7c1b98897b86a2a812573
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-09-26 23:46:57 -0700

    update header & create dynamics for hamburger

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,9 +1,20 @@
-import "./header.scss"
+import "./header.scss";
 
-export default function Header() {
+export default function Header ({ menuOpen, setMenuOpen }) {
     return (
-        <div className="header" id="header">
-            
+        <div className={"header " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#header" className="logo">fire.</a>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
