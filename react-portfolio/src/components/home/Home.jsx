5d7423539aaa66a9b6286ea6dd8a58e1b05ce628
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-09-28 23:39:26 -0700

    add white color to left side & black to the right

--- react-portfolio/src/components/home/Home.jsx
+++ react-portfolio/src/components/home/Home.jsx
@@ -3,7 +3,8 @@ import "./home.scss"
 export default function Home() {
     return (
         <div className="home" id="home">
-            home
+            <div className="left"></div>
+            <div className="right"></div>
         </div>
     )
 }
