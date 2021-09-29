c5168ebc61c4ed46f0884b483af65ccfa61a6288
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-09-28 23:50:07 -0700

    update text

--- react-portfolio/src/components/home/Home.jsx
+++ react-portfolio/src/components/home/Home.jsx
@@ -3,9 +3,9 @@ import "./home.scss"
 export default function Home() {
     return (
         <div className="home" id="home">
-            home
+            
             <div className="left"></div>
-            <div className="right"></div>
+            <div className="right">HOME</div>
         </div>
     )
 }
