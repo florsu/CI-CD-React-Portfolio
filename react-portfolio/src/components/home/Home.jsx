0a7226e77ba07f4e65eefd076910c7c0f795e622
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-09-28 23:43:02 -0700

    add text

--- react-portfolio/src/components/home/Home.jsx
+++ react-portfolio/src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import "./home.scss"
 export default function Home() {
     return (
         <div className="home" id="home">
+            home
             <div className="left"></div>
             <div className="right"></div>
         </div>
