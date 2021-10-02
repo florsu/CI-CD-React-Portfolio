db9c5edcc2b3fd9a9089411211763d70cd123f3c
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-10-01 19:15:14 -0700

    update dynamics in home

--- react-portfolio/src/components/home/Home.jsx
+++ react-portfolio/src/components/home/Home.jsx
@@ -1,11 +1,40 @@
 import "./home.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Home() {
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            showCursor: true,
+            strings: ["Developer", "Designer"],
+        });
+    }, []);
+
     return (
         <div className="home" id="home">
-            
-            <div className="left"></div>
-            <div className="right">HOME</div>
+
+            <div className="left">
+                <div className="imgContainer container-fluid">
+                    <img src="assets/my-image.jpg" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                <h2>Hello, I'm</h2>
+                <h1>Christian Flores</h1>
+                <h3>
+                    Freelance <span ref={textRef}>Designer</span>
+                    </h3>
+            </div>
+            <a href="#portfolio">
+                <img src="assets/down.png" alt="" />
+            </a>
         </div>
+    </div>
     )
 }
