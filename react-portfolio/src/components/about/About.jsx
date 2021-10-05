01d1c5e4b674e5102c86d492351b75a6521c7949
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-10-05 01:49:56 -0700

    update about me

--- react-portfolio/src/components/about/About.jsx
+++ react-portfolio/src/components/about/About.jsx
@@ -11,9 +11,10 @@ export default function About() {
             </div>
             <div className="right">
                 <div className="wrapper">
-                    <p>I am a solutions-driven Front-end Developer with a passion for UX/UI design based in San Francisco, California.
-                          I have tried different kind of careers,however, all of them are not fulfilling until I discovered Web Development.
-                           Having an innovative freedom on this profession makes me feel accomplished regardless  of the complexity that comes with it.I also have a background in customer service from my past jobs and earned a certificate in Front-End Web Development from the University of California, Riverside under Amazon Career Choice. Everyday, I get excited of something new to learn and I am naturally curious and never-ending improving my creative side.
+                    <p>I am a solution-driven Developer with a passion for UX/UI design based in San Francisco, California.
+                       I have tried different kind of careers, however, none of those gave me the fulfillment which I found in Web Development.
+                       Having an innovative freedom on this profession makes me feel accomplished and have a strong impact regardless of the complexity that comes with it.I also have a background in customer service and earned a certificate in Front-End Web Development Bootcamp from the University of California, Riverside under Amazon Career Choice.
+                       Everyday, I get excited of something new to learn and I am naturally curious and never-ending improving my creative side.
 </p>
                 </div>
             </div>
