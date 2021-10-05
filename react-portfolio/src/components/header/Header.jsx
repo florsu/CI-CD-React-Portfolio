95ebd0295a99a83dfce199062a16a9203ce31966
Christian G.  Flores <Christiangflores0824@gmail.com>
2021-10-05 01:29:26 -0700

    update code

--- react-portfolio/src/components/header/Header.jsx
+++ react-portfolio/src/components/header/Header.jsx
@@ -1,20 +1,22 @@
 import "./header.scss";
 
-export default function Header ({ menuOpen, setMenuOpen }) {
-    return (
-        <div className={"header " + (menuOpen && "active")}>
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#home" className="logo">thrive</a>
-                </div>
-                <div className="right">
-                    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-                        <span className="line1"></span>
-                        <span className="line2"></span>
-                        <span className="line3"></span>
-                    </div>
-                </div>
-            </div>
+export default function Header({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"header " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#home" className="logo">
+            thrive
+          </a>
         </div>
-    )
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
